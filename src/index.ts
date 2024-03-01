e2460eb656704e8ef8ab4828b9d595002b209ba9
g1769831 <2514874229@qq.com>
2024-03-01 17:05:31 +0800

    npm方法库

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+/**
+ * @description 日期格式化
+ * @param {*} time 
+ * @param {*} pattern 
+ * @returns 
+ */
+export function parseTime(time: Date | string | number, pattern?: string) {
+  if (arguments.length === 0 || !time) {
+    return null;
+  }
+  const format = pattern || "{y}-{m}-{d} {h}:{i}:{s}";
+  let date: Date | null = null;
+  if (time instanceof Date) {
+    date = time;
+  } else {
+    if (typeof time === "string" && /^[0-9]+$/.test(time)) {
+      time = parseInt(time);
+    } else if (typeof time === "string") {
+      time = time
+        .replace(new RegExp(/-/gm), "/")
+        .replace("T", " ")
+        .replace(new RegExp(/\.[\d]{3}/gm), "");
+    }
+    if (typeof time === "number" && time.toString().length === 10) {
+      time = time * 1000;
+    }
+    date = new Date(time);
+  }
+
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  };
+  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+    console.log(key);
+    let value: number | string = formatObj[key as keyof typeof formatObj];
+    // Note: getDay() returns 0 on Sunday
+    if (key === "a") {
+      return ["日", "一", "二", "三", "四", "五", "六"][value];
+    }
+    if (result.length > 0 && value < 10) {
+      value = "0" + value;
+    }
+    return (value || 0) as string;
+  });
+  return time_str;
+}
